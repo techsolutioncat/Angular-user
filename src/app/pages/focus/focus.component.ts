4acf8556c1c23dfa0fb7ee79e85353303cb888c7
techsolutioncat <steven.penzeve130@gmail.com>
2024-03-16 08:42:30 -0400

    Finish focus page

--- src/app/pages/focus/focus.component.ts
+++ src/app/pages/focus/focus.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HeaderComponent } from '../layout/header/header.component';
 import { FooterComponent } from '../layout/footer/footer.component';
 import { faEdit, faRemove } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'focus-root',
   standalone: true,
-  imports: [FontAwesomeModule, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, FontAwesomeModule],
   templateUrl: './focus.component.html',
   styleUrl: './focus.component.scss',
 })
@@ -21,4 +22,5 @@ export class FocusComponent {
   //Import Fontawesome
   faEdit = faEdit;
   faRemove = faRemove;
+  page: string = 'Focus Point';
 }
--- src/app/pages/layout/header/header.component.ts
+++ src/app/pages/layout/header/header.component.ts
@@ -1,13 +1,72 @@
-import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, Renderer2, Input } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faNavicon, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'header-root',
   standalone: true,
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss',
+  styleUrls: ['../../auth/contents/layout/header/header.component.scss', './header.component.scss'],
 })
 
 export class HeaderComponent {
-  //Import Social icons
-  image: string = '../../../../../assets/images/header-image.png'; // Replace with your image URL
+  @Input() page: any;
+
+  // svg
+  logo: string = '../../../assets/images/svg/logo.svg';
+  MobileLogo: string = '../../../assets/images/svg/mobile-logo.svg';
+  gold: string = '../../../assets/images/svg/gold.svg';
+  homepage: string = '../../../assets/images/svg/homepage.svg';
+  focus: string = '../../../assets/images/svg/focus.svg';
+  rewards: string = '../../../assets/images/svg/rewards.svg';
+  profile: string = '../../../assets/images/svg/profile.svg';
+  statistics: string = '../../../assets/images/svg/statistics.svg';
+  logout: string = '../../../assets/images/svg/logout.svg';
+  megamenu: string = '../../../assets/images/svg/megamenu.svg';
+
+  // Define an array of objects
+  items: { name: string, svg: string, liClass: string }[] = [
+    { name: 'Homepage', svg: this.homepage, liClass: 'nav-link d-flex ls-none' },
+    { name: 'Focus Point', svg: this.focus, liClass: 'nav-link d-flex ls-none' },
+    { name: 'Rewards', svg: this.rewards, liClass: 'nav-link d-flex ls-none' },
+    { name: 'Profile', svg: this.profile, liClass: 'nav-link d-flex ls-none' },
+    { name: 'My Statistics', svg: this.statistics, liClass: 'nav-link d-flex ls-none' },
+    { name: 'Pricing', svg: this.gold, liClass: 'nav-link d-flex ls-none' }
+  ];
+
+  //Import Fontawesome
+  faNavicon = faNavicon;
+  faXmark = faXmark;
+
+  constructor(private router: Router, private renderer: Renderer2) { }
+  showMobileMenu: boolean = false;
+  toggleMobileMenu() {
+    this.showMobileMenu = !this.showMobileMenu;
+    const closemenu = document.getElementById('closemenu');
+    const openmenu = document.getElementById('openmenu');
+    const MbileMenuList = document.getElementById('MbileMenuList');
+
+    if (this.showMobileMenu) {
+      this.renderer.setStyle(closemenu, 'display', 'block');
+      this.renderer.setStyle(openmenu, 'display', 'none');
+      this.renderer.setStyle(MbileMenuList, 'display', 'block');
+    } else {
+      this.renderer.setStyle(closemenu, 'display', 'none');
+      this.renderer.setStyle(openmenu, 'display', 'block');
+      this.renderer.setStyle(MbileMenuList, 'display', 'none');
+    }
+  };
+
+  loopOperation(): void {
+    console.log('item');
+
+    this.items.forEach(item => {
+      if(item.name == this.page) {
+        item.liClass = 'nav-link d-flex ls-none active';
+      }
+    });
+  }
 }
