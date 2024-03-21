207091950ff96f49300bc296470fb2ee96574e11
techsolutioncat <steven.penzeve130@gmail.com>
2024-03-21 14:19:55 -0400

    Check registed user by email

--- src/app/pages/auth/contents/signup/signup.component.ts
+++ src/app/pages/auth/contents/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Router } from '@angular/router';
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { SubcontComponent } from '../layout/subcont/subcont.component';
 import { HeaderComponent } from '../layout/header/header.component';
 
@@ -7,7 +8,7 @@ import { HeaderComponent } from '../layout/header/header.component';
   selector: 'login-root',
   standalone: true,
   templateUrl: './signup.component.html',
-  imports: [HeaderComponent, SubcontComponent],
+  imports: [HttpClientModule, HeaderComponent, SubcontComponent],
   styleUrls: ['../scss/auth.component.scss', '../scss/input.component.scss', '../scss/button.component.scss'],
 })
 
@@ -23,14 +24,34 @@ export class SignupComponent {
   mail: string = '../../../../../assets/images/svg/mail.svg';
   phone: string = '../../../../../assets/images/svg/phone.svg';
   user: string = '../../../../../assets/images/svg/user.svg';
-  
+
   title = 'Create Account';
   subtitle = 'Provide necessary information to proceed with registration or sign up with social media';
 
-  constructor(private router: Router) { }
-  navigateToComponent(page: string, username: string, email: string, phone: string ) {
-    if( username.trim() && email.trim() && phone.trim()) {
-      this.router.navigate([page, { username: username, email:email, phone:phone }]); // Navigate to signup
+  private apiUrl = 'http://localhost:3000/users';
+  @ViewChild('msgElement') msgElement!: ElementRef;
+
+  constructor(private router: Router, private http: HttpClient) { }
+  navigateToCreatePassword(page: string, event: Event, username: string, email: string, phone: string) {
+    if (username.trim() && email.trim() && phone.trim()) {
+      event.preventDefault(); // Prevent the default form submission behavior
+      const url = `${this.apiUrl}/${email.trim()}`;
+      this.http.get<any>(url)
+        .subscribe(response => {
+          if (response) {
+            this.msgElement.nativeElement.style.display = 'block';
+          } else {
+            this.router.navigate([page, { username: username, email: email, phone: phone }]); // Navigate to signup
+          }
+          // Handle success or show a success message
+        }, error => {
+          console.error('Error sending user data:', error);
+          // Handle error or show an error message
+        });
     }
+  }
+
+  navigateToLogin(page: string) {
+    this.router.navigate([page]); // Navigate to signup
   }
 }
