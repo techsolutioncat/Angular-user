dc2de85b57a94298e0e0956c39c50a0450640ced
techsolutioncat <steven.penzeve130@gmail.com>
2024-03-24 15:19:26 -0400

    Finish 404page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/auth/contents/login/login.component';
+import { NotfoundComponent } from './pages/404/404.component';
 import { SignupComponent } from './pages/auth/contents/signup/signup.component';
 import { CreatePasswordComponent } from './pages/auth/contents/createpassword/createpassword.component';
 import { FocusComponent } from './pages/focus/focus.component';
@@ -10,9 +11,13 @@ export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'createpassword', component: CreatePasswordComponent },
+  { path: '404', component: NotfoundComponent },
   { path: 'focus', component: FocusComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   // Add more routes as needed
+
+  // Redirect all unknown routes to the home page
+  { path: '**', redirectTo: '/404' },
 ];
 
 @NgModule({
---
+++ src/app/pages/404/404.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  templateUrl: './404.component.html',
+  styleUrl: './404.component.scss'
+})
+export class NotfoundComponent {}
